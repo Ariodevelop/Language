c0419459c123146bcd6706a45d3bbc4d854d6215
Ariodevelop <owner@ariodevelop.ir>
2022-09-20 11:37:32 +0430

    develop project

--- source/2-grammer.ts
+++ source/2-grammer.ts
@@ -1,35 +1,15 @@
 const GRAMMER = {
   function: [
-    {
-      type: "word",
-    },
-    {
-      type: "operator",
-      data: ":",
-    },
-    {
-      type: "word",
-      most:types,
-      check: 12,
-    },
-    {
-      type: "word",
-      require: true,
-    },
-    {
-      type: "operator",
-      data: ",",
-      check: 0,
-    },
-    {
-      type: "word",
-      require: true,
-    },
-    {
-      type: "word",
-      require: true,
-      loop:12
-    },
+    { type: "word" },
+    { type: "operator", data: ":" },
+    { type: "word", most:types, check: 7 },
+    { type: "word", require: true },
+    { type: "operator", data: ",", check: 0 },
+    { type: "word", require: true },
+    { type: "word", require: true, loop:4 },
+    { type: "open", require: true, data:"{"},
+    { type: "body"},
+    { type: "close", require: true, data:"}"},
   ],
 };
 
