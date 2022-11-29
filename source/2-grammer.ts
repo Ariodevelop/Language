d4736d501d9b6991e05d3e433ebdcc987ffbc450
ariodevelop <owner@ariodevelop.ir>
2022-11-29 21:15:16 +0330

    developing

--- source/2-grammer.ts
+++ source/2-grammer.ts
@@ -1,15 +1,25 @@
-const GRAMMER = {
-  function: [
-    { type: "word" },
-    { type: "operator", data: ":" },
-    { type: "word", most:types, check: 7 },
-    { type: "word", require: true },
-    { type: "operator", data: ",", check: 0 },
-    { type: "word", require: true },
-    { type: "word", require: true, loop:4 },
-    { type: "open", require: true, data:"{"},
-    { type: "body"},
-    { type: "close", require: true, data:"}"},
-  ],
-};
+const GRAMMER = [
+  {
+    _name:"function",
+    structure: [
+      { type: "word" },
+      { type: "operator", data: ":" },
+      { type: "word", most: types, check: 7 },
+      { type: "word", require: true },
+      { type: "operator", data: ",", check: 0 },
+      { type: "word", require: true },
+      { type: "word", require: true, loop: 4 },
+      { type: "open", require: true, data: "{" },
+      { type: "body" },
+      { type: "close", require: true, data: "}" },
+    ],
+  },
+  {
+    _name:"variable",
+    structure: [
+      { type: "word", most: types },
+      { type: "word", require: true },
+    ],
+  },
+];
 
